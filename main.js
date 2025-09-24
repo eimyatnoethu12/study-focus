13b0e021faa1c6963ae2787d1d5ee4fcee0cd9f2
eimyatnoethu12 <eimyatnoethu1003@gmail.com>
2025-09-24 20:02:40 +0630

    add

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-let mainWindow;
+let mainWindow ;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
